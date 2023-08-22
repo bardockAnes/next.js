177dd1f75a1af1b7df3ba75d99c9e6c07db219c8
bardockAnes <139508042+bardockAnes@users.noreply.github.com>
2023-08-22 04:16:26 +0200

    nextjs deploy

---
+++ bardockanes/app/page.js
@@ -0,0 +1,8 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+   <div style={{display:"flex",alignItems:"center",justifyContent:"center",alignContent:"center",fontSize:"60px",color:"white",fontFamily:"cursive"}}>wellcome back </div>
+  )
+}
